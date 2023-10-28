cbf4532a7f55ab16eb050551d542a238a85cd051
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-28 12:03:36 +0700

    initial commit

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskManager(
+                        getString(R.string.first_text),
+                        getString(R.string.second_text)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskManager(firstText: String, secondText: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = firstText,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+        )
+        Text(
+            text = secondText,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        TaskManager(
+            "All tasks completed",
+            "Nice work!"
+        )
+    }
+}
